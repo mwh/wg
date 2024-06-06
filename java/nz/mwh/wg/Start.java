f78806d9bc13ea389b5219b2e12275b6719fdf3b
Michael Homer <mwh@ecs.vuw.ac.nz>
2024-06-06 19:00:40 +1200

    Add file-updating option to Java version
    The -u file option will update a line containing "program =" with the AST of the source file being processed.

--- java/nz/mwh/wg/Start.java
+++ java/nz/mwh/wg/Start.java
@@ -3,6 +3,9 @@ package nz.mwh.wg;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 import nz.mwh.wg.ast.ASTNode;
 
@@ -10,9 +13,14 @@ public class Start {
     public static void main(String[] args) {
         String filename = "test.grace";
         boolean printAST = false;
+        String updateFile = null;
         for (String arg : args) {
             if (arg.equals("-p")) {
                 printAST = true;
+            } else if (arg.equals("-u")) {
+                updateFile = "";
+            } else if (updateFile != null && updateFile.isEmpty()) {
+                updateFile = arg;
             } else {
                 filename = arg;
                 break;
@@ -23,11 +31,40 @@ public class Start {
             ASTNode ast = Parser.parse(source);
             if (printAST) {
                 System.out.println(ast);
+            } else if (updateFile != null) {
+                updateFile(ast, updateFile);
             } else {
                 Evaluator.evaluateProgram(ast);
             }
         } catch (IOException e) {
             throw new RuntimeException("Error reading file: " + filename);
+        }
+    }
+
+    private static void updateFile(ASTNode ast, String filename) {
+        try {
+            List<String> source = Files.readAllLines(Path.of(filename));
+            List<String> newSource = new ArrayList<>();
+            Pattern pattern = Pattern.compile("^(|.*\\s)program\\s*=");
+            System.out.println(pattern);
+            boolean foundMatch = false;
+            for (String line : source) {
+                if (pattern.matcher(line).lookingAt()) {
+                    int pos = line.indexOf("program");
+                    String newLine = line.substring(0, pos + 7) + " = " + ast.toString() + (line.endsWith(";") ? ";" : "");
+                    newSource.add(newLine);
+                    System.out.println("Found program line");
+                    foundMatch = true;
+                } else {
+                    newSource.add(line);
+                }
+            }
+            if (!foundMatch) {
+                throw new RuntimeException("No program line found in file: " + filename);
+            }
+            Files.writeString(Path.of(filename), String.join("\n", newSource));
+        } catch (IOException e) {
+            throw new RuntimeException("Error writing file: " + filename);
         }
     }
 }
