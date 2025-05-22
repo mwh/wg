b668686ed82aba3de29303e4c867002cce6f2ab5
Michael Homer <mwh@ecs.vuw.ac.nz>
2025-05-23 11:45:23 +1200

    Make Haskell able to run programs from CLI
    With no arguments, the compiled executable will run the hard-coded
    program. With a single argument "dump", prints the serialised AST of the
    hard-coded program.
    
    If the hard-coded program is the parser, additional command-line modes
    are usable:
    * main run <PATH>: executes the Grace program in the file named by PATH
    * main eval <CODE-STRING>: executes the Grace code in CODE-STRING
    * main dumpFile <PATH>: prints serialised AST of Grace file PATH
    * main dump <CODE-STRING>: prints serialised AST of Grace CODE-STRING

--- haskell/main.hs
+++ haskell/main.hs
@@ -2,46 +2,55 @@ import Program
 import Ast (prettyPrint)
 import CPS (toFunc)
 import Runtime
+import System.Environment (getArgs)
 
 main =
     do
-        let func = toFunc program
-        func dropContext
+        args <- getArgs
+        case args of
+            [] -> do
+                let func = toFunc program
+                func dropContext
+            ["run", path] -> do
+                contents <- readFile path
+                parseExecute contents
+            ["eval", code] ->
+                parseExecute code
+            ["dump"] ->
+                dump
+            ["dumpFile", path] -> do
+                contents <- readFile path
+                processAST contents (\val -> case val of GraceAstObject n -> putStrLn $ prettyPrint n)
+            ["dump", code] ->
+                processAST code (\val -> case val of GraceAstObject n -> putStrLn $ prettyPrint n)
+            _ -> do
+                putStrLn "Invalid arguments. Use no arguments to run hard-coded program, or if parser is compiled in, 'run <path>' to run a file, or 'eval <code>' to evaluate a string."
 
 dump =
     putStrLn $ prettyPrint program
 
 
 -- When program = the parser, this will allow running the parser on
--- an arbitrary string from the REPL.
-runParse s =
+-- arbitrary code, and then doing something with the result.
+processAST code using =
     do
         let func = toFunc program
         func $ resultContext $ \val ->
             let parse = getMethod "parse(1)" val
-                string = GraceString s
+                string = GraceString code
             in
-                parse (resultContext (\result ->
+                parse (resultContext using) [string]
+
+runParse s = processAST s $ (\result ->
                     do
                         let asString = getMethod "asString(0)" result
                         asString printContext []
-                        --putStrLn $ "Result: " ++ (show result)
-                        --return ()
                     )
-                ) [string]
 
-parseExecute s =
-    do
-        let func = toFunc program
-        func $ resultContext $ \val ->
-            let parse = getMethod "parse(1)" val
-                string = GraceString s
-            in
-                parse (resultContext (\result ->
+parseExecute s = processAST s $ (\result ->
                     do
                         case result of
                             GraceAstObject n -> do
                                 toFunc n $ dropContext
                             _ -> putStrLn $ "Parse result was not an AST object: " ++ (show result)
                     )
-                ) [string]
